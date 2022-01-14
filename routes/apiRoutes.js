a8b9b56639587d5c07fc1b03c98e42d5b413e1aa
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-01-14 16:14:58 -0500

    added routes files and made server.js modifications

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const db = require("../db/db.json");
+
+router.get("/notes", (req, res) => {
+
+});
+
+router.post("/notes", (req, res) => {
+
+});
+
+router.delete("/notes/:id", (req, res) => {
+    
+});
+
+module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const router = require("express").Router();
+
+// GET Route for homepage
+router.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+router.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+module.exports = router;
